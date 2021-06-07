92f0a3c46de7a027b7369047f0b5efee2bbbd5dd
Erik Reinert <e@drn.io>
2021-06-06 17:36:41 -0700

    feat(new): setup simple CLI framework

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new("Tasks")
+        .version("0.1.0")
+        .author("Erik Reinert")
+        .about("Super awesome task management tool")
+        .subcommand(
+            App::new("new")
+                .about("Creates a new command")
+                .version("0.1.0")
+                .author("Erik Reinert")
+                .arg(
+                    Arg::new("pomodoro")
+                        .long("pomodoro")
+                        .short('p')
+                        .about("Set desired pomodoro amount"),
+                ),
+        )
+        .get_matches();
+
+    if let Some(ref matches) = matches.subcommand_matches("new") {
+        if matches.is_present("pomodoro") {
+            println!("lets set a custom pomodoro")
+        }
+    }
+}
